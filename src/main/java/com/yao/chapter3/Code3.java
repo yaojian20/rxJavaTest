2969b32cd3d7cde035d4b7658eff986203f4a747
Yao jian <jian.yao@eisgroup.com>
2022-05-25 18:06:45 +0800

    RxJava

--- src/main/java/com/yao/chapter3/Code3.java
+++ src/main/java/com/yao/chapter3/Code3.java
@@ -1,4 +1,19 @@
 package com.yao.chapter3;
 
+import com.yao.superheroes.SuperHeroesService;
+import io.vertx.core.json.JsonObject;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+
 public class Code3 {
+
+    public static void main(String[] args) {
+        SuperHeroesService.run();
+
+        SuperHeroesService.client().get("/heroes").rxSend()
+                .map(HttpResponse::bodyAsJsonObject)
+                .map(JsonObject::size)
+                .subscribe(length -> System.out.println("Number of heroes: " + length));
+    }
+
+
 }
--- src/main/java/com/yao/superheroes/SuperHeroesService.java
+++ src/main/java/com/yao/superheroes/SuperHeroesService.java
@@ -1,14 +1,19 @@
 package com.yao.superheroes;
 
 import io.reactivex.Completable;
-import io.vertx.core.Vertx;
-import io.vertx.ext.web.Router;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.client.WebClient;
 
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SuperHeroesService {
 
+    private final static Vertx vertx = Vertx.vertx();
 
     //详细
     private final boolean verbose;
@@ -27,7 +32,44 @@ public class SuperHeroesService {
         Vertx vertx = Vertx.vertx();
         //创建一个路由对象
         Router router = Router.router(vertx);
-        return null;
+        //绑定路由地址及处理方法
+        router.route("/heroes").handler(this::getAllHeroes);
+
+        //读取文件
+        return vertx.fileSystem().rxReadFile("src/main/resources/characters.json")
+                //将json文件里的数据转化成character
+                .map( buffer -> buffer.toJsonArray().stream().map( jsonObject -> new Character((JsonObject) jsonObject)).collect(Collectors.toList()))
+                .doOnSuccess( list -> {
+                    //过滤恶棍，并按id放入map中
+                    this.villains = list.stream().filter(Character::isVillain).collect(
+                            HashMap::new,(map,character) -> map.put(character.getId(),character),HashMap::putAll);
+                    //过滤英雄，并按id放入map中
+                    this.heroes = list.stream().filter(e -> ! e.isVillain()).collect(
+                            HashMap::new, (map, character) -> map.put(character.getId(), character), HashMap::putAll);
+                })
+                //转成成一个http请求，端口号8080
+                .flatMap(x -> vertx.createHttpServer()
+                        .requestHandler(router::accept)
+                        .rxListen(8080))
+                .toCompletable();
+    }
+
+    public void getAllHeroes(RoutingContext routingContext){
+            routingContext.response().end(villains.values().stream()
+                    .collect(JsonObject::new,
+                            (json, superStuff) -> json.put(Integer.toString(superStuff.getId()), superStuff.getName()),
+                            JsonObject::mergeIn)
+                    .encodePrettily());
+    }
+
+    public static void run() {
+        new SuperHeroesService(true).start().blockingAwait();
+    }
+
+    public static WebClient client() {
+        return WebClient.create(vertx,
+                new WebClientOptions().setDefaultPort(8080).setDefaultHost("localhost")
+        );
     }
 
 }
